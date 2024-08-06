091ed404c2e5ebbec0e9857ee2d91fe1ec51ef24
Tyler Van Gorder <tyler.vangorder@8451.com>
2024-08-06 08:41:00 -0700

    Add a shutdown hook to make sure we are getting a SIGTERM on docker container shutdown.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			//This logging line should show up when the docker container is stopped!
+			System.out.println("Shutting down Gracefully, BYE!");
+		}));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
